0e3228d559a2a99010853d74222043a1b6798acb
Mette Vorraa <mettevorraa@gmail.com>
2021-09-29 18:30:47 +0100

    move api search data

--- src/Forecast.js
+++ src/Forecast.js
@@ -9,30 +9,21 @@ export default function Forecast(props) {
 const [ready, setReady] = useState(false);
 const [forecast, setForecast] = useState(null);
 
-function forecastSearch () {
-    let apiKey = "de31873c66b8933cfbbc1e0df416d91d";
-    let lon = props.coordinates.lon;
-    let lat = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleForecast);
-}
-    
-
 
 function handleForecast(response) {
     setForecast(response.data.daily);
     setReady(true);
- 
+    
 }
 
-    if (ready) {
+if (ready) {
     return (
       <div className="Forecast">
       <div className="row">
 		{forecast.map(function (dailyForecast, index) {
-		    if (index < 4) {
-			return (  
-                <div className="col" key={index}>
+            if (index < 4) {
+                return (  
+                    <div className="col" key={index}>
                 <ForecastDaily data={dailyForecast}/>
                 </div>
  );
@@ -42,7 +33,11 @@ function handleForecast(response) {
 </div>
 );    
 } else {
-        forecastSearch();
-        return "Loading";
+    let apiKey = "de31873c66b8933cfbbc1e0df416d91d";
+    let lon = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleForecast);
+    return "Loading";
     }
 }
